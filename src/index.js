a47fa2c963ba26e86d9581402b1538da4dc2688f
CbnMarian <m.marian03@yahoo.com>
2023-12-11 00:33:18 +0200

    added data

--- src/index.js
+++ src/index.js
@@ -2,9 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <div>
+      {" "}
+      <h1>Hello</h1>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+function Pizza() {
+  return (
+    <div>
+      <img src="/pizzas/spinaci.jpg" alt="pizza spinaci" />
+      <h2>Pizza Spinaci</h2>
+      <p>Tomato, mozarella, ham, aragula, and burrata cheese</p>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
